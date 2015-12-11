9cfced4509f84eb58ceead57a03089c3d0c85d3b
Arman Jalalian <arbi413@gmail.com>
2015-12-11 12:15:45 +0100

    Refactor html

--- dino.js
+++ dino.js
@@ -10,7 +10,6 @@ function hideClass(name) {
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//test
 (function() {
     'use strict';
     /**
